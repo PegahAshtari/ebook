45de8a817ee34b1c7e800b3329d34f474282ec25
mrs.alpha <pegahashtari1997@gmail.com>
2024-12-04 11:40:46 +0330

    add countdown

--- js/script.js
+++ js/script.js
@@ -1,14 +1,36 @@
-// نمایش دکمه در هنگام اسکرول پایین صفحه
+window.onload = function () {
+    // شمارش معکوس برای هر عدد
+    function startCountdown(id, targetNumber) {
+        let currentNumber = 0;
+        const element = document.getElementById(id);
+
+        const interval = setInterval(() => {
+            if (currentNumber < targetNumber) {
+                currentNumber++;
+                element.textContent = currentNumber;
+            } else {
+                clearInterval(interval);
+            }
+        }, 0.5); // زمان بین هر افزایش (برای سرعت بالاتر می‌توانید مقدار 10 را تغییر دهید)
+    }
+
+    // شمارش معکوس برای هر بخش
+    startCountdown('countdown-1', 3); // عدد 3 برای بخش اول
+    startCountdown('countdown-2', 170000); // عدد 170000 برای بخش دوم
+    startCountdown('countdown-3', 86); // عدد 86 برای بخش سوم
+    startCountdown('countdown-4', 700); // عدد 700 برای بخش چهارم
+};
+
 window.onscroll = function () {
     let scrollToTopBtn = document.getElementById("scrollToTop");
-    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
         scrollToTopBtn.style.display = "block";
     } else {
         scrollToTopBtn.style.display = "none";
     }
 };
 
-// اسکرول به سکشن #top-view هنگام کلیک بر روی دکمه
+
 document.getElementById("scrollToTop").addEventListener("click", function () {
     document.getElementById("top-view").scrollIntoView({
         behavior: "smooth"
